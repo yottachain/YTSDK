f00b02567450307670c84566dc3b5ce29268f41a
liushepeng <liushepeng@sohu.com>
2019-05-16 13:10:48 +0800

    YTSDK

---
+++ src/main/java/com/ytfs/client/s3/ObjectHandler.java
@@ -0,0 +1,12 @@
+package com.ytfs.client.s3;
+
+import com.ytfs.common.ServiceException;
+import org.bson.types.ObjectId;
+
+public class ObjectHandler {
+
+    public static void createObject(String bucketname, String filename, ObjectId VNU) throws ServiceException {
+      
+    }
+        
+}
