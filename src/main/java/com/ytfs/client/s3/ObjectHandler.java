271cea7bf3d780fffec1d275d5c5d179e8c6c311
LAPTOP-IM0URKF0\phs <penghuaisong@sursen.net>
2019-05-16 13:19:38 +0800

    test

--- src/main/java/com/ytfs/client/s3/ObjectHandler.java
+++ src/main/java/com/ytfs/client/s3/ObjectHandler.java
@@ -1,12 +1,20 @@
 package com.ytfs.client.s3;
 
 import com.ytfs.common.ServiceException;
+import com.ytfs.common.conf.UserConfig;
+import com.ytfs.common.net.P2PUtils;
+import com.ytfs.service.packet.s3.ListBucketReq;
+import com.ytfs.service.packet.s3.ListBucketResp;
 import org.bson.types.ObjectId;
 
 public class ObjectHandler {
 
     public static void createObject(String bucketname, String filename, ObjectId VNU) throws ServiceException {
-      
+
     }
-        
+    public static String[] listObject(String bucketName) throws ServiceException {
+        ListBucketReq req = new ListBucketReq();
+        ListBucketResp resp = (ListBucketResp) P2PUtils.requestBPU(req, UserConfig.superNode);
+        return resp.getNames();
+    }
 }
