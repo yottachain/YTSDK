5f1ef5e9a979c71e825e6db2e585776cdbe5432e
liushepeng <liushepeng@sohu.com>
2019-05-16 09:33:27 +0800

    YTSDK

---
+++ bin/sdktest.bat
@@ -0,0 +1,25 @@
+@echo off 
+
+rem ---------在这里设置要上传的文件路径------------
+
+set filepath=d:/aa.txt
+
+
+rem --------------------upload test----------------------
+
+setlocal enabledelayedexpansion
+for /f "tokens=1,2,3 delims==" %%i in (ytfs.conf) do (
+ if "%%i"=="wrapper.java.command" set java_cmd=%%j
+ if "%%i"=="wrapper.java.initmemory" set java_opts=-Xms%%jM
+ if "%%i"=="wrapper.java.maxmemory" set java_opts=!java_opts! -Xmx%%jM
+ if "%%i"=="wrapper.java.additional.2" set java_opts=!java_opts! %%j=%%k
+ if "%%i"=="wrapper.java.classpath.1" set classpath=%%j
+)
+set java_opts=!java_opts! -Dfile.encoding=GBK
+
+set mainclass=com.ytfs.client.examples.SDKTest
+set java_cmd=%java_cmd:/=\%
+set cmd=%java_cmd% %java_opts% -classpath %classpath% %mainclass% %filepath%
+echo cmd: %cmd%
+echo enter 'x' to shutdown server, 'r' to restart server ...
+%cmd%
---
+++ bin/sdktest.sh
@@ -0,0 +1,37 @@
+# ! /bin/sh
+# ------------在这里设置要上传的文件路径---------------------
+filepath=/root/test.dat
+
+# ---------------------upload test-----------------------
+source ytfs.ev
+
+if [ -z $YTFS_HOME ]; then  
+    echo "Environment variable 'YTFS_HOME' not found "
+    exit 0;
+fi 
+
+echo "YTFS_HOME:$YTFS_HOME"
+cd $YTFS_HOME
+
+while  IFS='=' read var val
+do
+    if [[ $var == 'wrapper.java.command' ]]
+    then
+         java_cmd=${val:0:${#val}-1}
+    elif [[ $var == 'wrapper.java.additional.1' ]]
+    then
+        java_opts="$java_opts ${val:0:${#val}-1}"
+    elif [[ $var == 'wrapper.java.additional.2' ]]
+    then
+        java_opts="$java_opts ${val:0:${#val}-1}"
+    elif [[ $var == 'wrapper.java.classpath.1' ]]
+    then
+        classpath=${val:0:${#val}-1}
+    fi 
+done < ytfs.conf
+ 
+mainclass="com.ytfs.client.examples.SDKTest"
+
+cmd="$java_cmd $java_opts -classpath $classpath $mainclass $filepath"
+echo "cmd: $cmd"
+$cmd
