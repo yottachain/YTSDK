edb1ca7842e140b84116d15e75ebffdc1af803b4
LAPTOP-IM0URKF0\phs <penghuaisong@sursen.net>
2019-05-17 15:45:41 +0800

    upload list object interface

--- src/main/java/com/ytfs/client/s3/ObjectHandler.java
+++ src/main/java/com/ytfs/client/s3/ObjectHandler.java
@@ -6,6 +6,8 @@ import com.ytfs.common.net.P2PUtils;
 import com.ytfs.service.packet.s3.*;
 import org.bson.types.ObjectId;
 
+import java.util.Map;
+
 public class ObjectHandler {
 
     public static void createObject(String bucketname, String filename, ObjectId VNU,byte[] meta) throws ServiceException {
@@ -16,9 +18,9 @@ public class ObjectHandler {
         req.setMeta(meta);
         P2PUtils.requestBPU(req, UserConfig.superNode);
     }
-    public static String[] listObject(String bucketName) throws ServiceException {
+    public static Map<String,byte[]> listObject(String bucketName) throws ServiceException {
         ListObjectReq req = new ListObjectReq();
         ListObjectResp resp = (ListObjectResp) P2PUtils.requestBPU(req, UserConfig.superNode);
-        return resp.getNames();
+        return resp.getMap();
     }
 }
