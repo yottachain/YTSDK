b3f752a2e94ebc48713f347bd1a7fcb39d49e513
liushepeng <liushepeng@yottachain.io>
2019-05-29 16:38:42 +0800

    SDK

--- src/main/java/com/ytfs/client/examples/SDKTest.java
+++ src/main/java/com/ytfs/client/examples/SDKTest.java
@@ -19,7 +19,7 @@ public class SDKTest implements WrapperListener {
     @Override
     public Integer start(String[] strings) {
         try {
-            strings = new String[]{"e:/ww.rar"};
+            strings = new String[]{"e:/nosqlbooster4mongo-5.1.5.0.rar"};
             ClientInitor.init();
             String filepath = null;
             String newfilepath = null;
