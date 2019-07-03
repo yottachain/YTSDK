1a3578443d78f5d1ca7195e8ce948070b5141fc7
liushepeng <liushepeng@sohu.com>
2019-07-03 13:33:27 +0800

    SDK

--- src/main/java/com/ytfs/client/examples/SDKTest.java
+++ src/main/java/com/ytfs/client/examples/SDKTest.java
@@ -21,7 +21,7 @@ public class SDKTest implements WrapperListener {
         try {
             ClientInitor.init();
             if (strings.length < 1) {
-                strings = new String[]{"d:/dcache-master.zip"};
+                strings = new String[]{"e:/ladon-s3-server.zip"};
             }
             String filepath = null;
             String newfilepath = null;
