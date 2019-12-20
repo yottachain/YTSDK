e85aadb748038a6fe4c6e1e50096b8c9c3e2f05f
Administrator <Administrator@7GAK325LVFDA2FZ>
2019-12-20 17:59:12 +0800

    SDK

--- src/main/java/com/ytfs/client/examples/SDKTest.java
+++ src/main/java/com/ytfs/client/examples/SDKTest.java
@@ -17,7 +17,7 @@ import org.tanukisoftware.wrapper.WrapperManager;
 public class SDKTest implements WrapperListener {
 
     private static final String sn = "yf";
-    private static final String path = "D:\\photoshop7.rar";
+    private static final String path = "D:\\test.rar";
 
     @Override
     public Integer start(String[] strings) {
